d54c91a80aeeed7a1ee27cd68f7367e50d978721
Abhijeet Kumar Patro <67703513+AbhijeetKumarPatro@users.noreply.github.com>
2026-04-02 15:22:39 +0530

    Change navbar logo from full name to AbhijeetOnline

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -23,7 +23,7 @@ export default function Navbar() {
         <div className="flex justify-between h-16">
           <div className="flex items-center">
             <Link href="/" className="text-xl font-bold text-gray-900">
-              Abhijeet Kumar Patro
+              AbhijeetOnline
             </Link>
           </div>
           <div className="flex items-center space-x-8">
